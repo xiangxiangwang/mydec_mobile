43b638ba2cc7c9181e8ecbb9212784be6b493125
Gary Zhang <garyzhangscm@gmail.com>
2020-11-30 10:46:03 -0800

    Add font change control to the QT page

--- lib/qt/routes/qt_info.dart
+++ lib/qt/routes/qt_info.dart
@@ -12,8 +12,7 @@ import 'package:mydec/i10n/localization_intl.dart';
 import 'package:mydec/qt/models/qt_info.dart';
 import 'package:video_player/video_player.dart';
 
-import '../../common/chewie_list_item.dart';
-
+import 'package:floating_action_row/floating_action_row.dart';
 
 
 
@@ -32,12 +31,23 @@ class _QTInfoPageState extends State<QTInfoPage> {
   TargetPlatform _platform;
   VideoPlayerController _videoPlayerController;
   ChewieController _chewieController;
+  double titleFontSize;
+  double subtitleFontSize;
+  double contentFontSize;
+
+
+  List<Widget> floatButtons = [];
 
   @override
   void initState() {
     super.initState();
     print("start to _QTInfoPageState with qt: ${widget.qtInfo.date} - ${widget.qtInfo.title}");
     this.initializePlayer();
+    _showDefaultFloatActionButtons();
+
+    titleFontSize = 20;
+    subtitleFontSize = 14;
+    contentFontSize = 14;
   }
 
   Future<void> initializePlayer() async {
@@ -100,6 +110,7 @@ class _QTInfoPageState extends State<QTInfoPage> {
   @override
   Widget build(BuildContext context) {
 
+
     return Scaffold(
         // appBar: AppBar(title: Text(DecLocalizations.of(context).dailyQt)),
 
@@ -110,25 +121,37 @@ class _QTInfoPageState extends State<QTInfoPage> {
                 children: <Widget>[_buildBody()]
             )
         ),
-        bottomNavigationBar: buildBottomNavigationBar(context)
+        bottomNavigationBar: buildBottomNavigationBar(context),
+        floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+        // floatingActionButton: _buildFloatingActionButton(),
+        floatingActionButton: FloatingActionRow(
+          children: floatButtons,
+          color: Colors.transparent,
+          elevation: 0,
+          height: 50,
+          shape: ContinuousRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+
+          )
+        ),
     );
   }
 
   Widget _buildBody() {
-    _platform = Theme.of(context).platform;
 
    return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(widget.qtInfo.title,
-          style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold)), //QT title bold
+          style:TextStyle(fontSize: titleFontSize ,fontWeight: FontWeight.bold)), //QT title bold
           Text(
-              widget.qtInfo.date, // QT date
+              widget.qtInfo.date,
+              style: TextStyle(fontSize: subtitleFontSize)// QT date
           ),
           _buildVideoPlayer(),
           Text(widget.qtInfo.content,
                 style: TextStyle(wordSpacing: 10,
-                    fontSize: 14,
+                    fontSize: contentFontSize,
                     letterSpacing: 2,
                       fontStyle: FontStyle.italic,),  ), // QT Main body
           ]
@@ -159,5 +182,75 @@ class _QTInfoPageState extends State<QTInfoPage> {
 
   }
 
+
+  _showDefaultFloatActionButtons() {
+    print("_showDefaultFloatActionButtons");
+    var buttons = List<Widget>();
+    buttons.add(FloatingActionButton(
+        elevation: 0.0,
+        child: Image.asset('assets/images/text_format_button.png') ,
+        backgroundColor: Colors.transparent,
+        onPressed: () => _showActionButtons(),
+    ));
+    /**
+    buttons.add(
+      FloatingActionRowButton(
+        icon: Icon(Icons.add),
+        onTap: () => _showActionButtons(),
+      ),
+    );
+        **/
+    print("start to show default action button");
+    setState(() {
+      floatButtons = buttons;
+    });
+  }
+  _showActionButtons() {
+    print("_showActionButtons");
+    var buttons = List<Widget>();
+    buttons.add(
+        FloatingActionButton(
+            elevation: 0.0,
+            child: Image.asset('assets/images/clear_text_format_button.png') ,
+            backgroundColor: Colors.transparent,
+            onPressed:() => _showDefaultFloatActionButtons(),
+        )
+    );
+    buttons.add(
+      FloatingActionRowDivider(),
+    );
+    buttons.add(
+        FloatingActionButton(
+          elevation: 0.0,
+          child: Image.asset('assets/images/decrease_font_size_cn.png') ,
+          backgroundColor: Colors.transparent,
+          onPressed:() => _changeFontSize(-1),
+
+        )
+    );
+    buttons.add(
+      FloatingActionRowDivider(),
+    );
+    buttons.add(
+        FloatingActionButton(
+          elevation: 0.0,
+          child: Image.asset('assets/images/increase_font_size_cn.png') ,
+          backgroundColor: Colors.transparent,
+          onPressed:() => _changeFontSize(1),
+        )
+    );
+    print("start to show _showActionButtons");
+    setState(() {
+      floatButtons = buttons;
+    });
+  }
+  _changeFontSize(int sizeChanged) {
+    setState(() {
+      titleFontSize += sizeChanged;
+      subtitleFontSize += sizeChanged;
+      contentFontSize += sizeChanged;
+    });
+
+  }
 }
 
